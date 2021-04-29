9bc9c0f2afda312a54242783ceae1b0250ab11d7
Antonio García <agarcia@shsconsultores.es>
2021-04-29 11:41:16 +0200

    creando módulo theme_georedia

---
+++ theme_georedia/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Theme GeoRedia',
+    'description': 'Tema para la web GeoRedia.com',
+    'version': '0.1',
+    'author': 'Antonio García - SHS Consultores',
+    'category': 'Theme/Corporate',
+
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+
+    ],
+}
