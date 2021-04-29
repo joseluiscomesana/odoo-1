00bb1993dc1d17287de4d6130aa6c6ce01ba6edc
José Luis <joseluis.comesana@Impulsa20>
2021-04-29 16:06:23 +0200

    modulo de prueba

---
+++ prueba/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "prueba",
+
+    'summary': """
+        Aplicación de prueba para comprobar el funcionamiento
+        de los módulos""",
+
+    'description': """
+        Sólo con propósitos de prueba
+    """,
+
+    'author': "Jfic",
+    'website': "http://www.georedia.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.12',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
