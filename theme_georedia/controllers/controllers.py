595c1b374cd0d408e3f06fd9cb9b95f0a23fed2c
Antonio García <agarcia@shsconsultores.es>
2021-05-03 13:20:04 +0200

    comment code to tests

--- theme_georedia/controllers/controllers.py
+++ theme_georedia/controllers/controllers.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-from odoo import http
+# # -*- coding: utf-8 -*-
+# from odoo import http
 
-class MyModule(http.Controller):
-    @http.route('/api/prueba/', auth='public')
-    def index(self, **kw):
-        return "Hello, world - " + http.request.params['nombre']
+# class MyModule(http.Controller):
+#     @http.route('/api/prueba/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world - " + http.request.params['nombre']
 
 
-    @http.route('/api/prueba', type='http', csrf=False, auth="public")
-    def update_order_webhook(self, **kwargs):
-    return Response(json.dumps({"yes":"i am json"}),content_type='application/json;charset=utf-8',status=200)
+#     @http.route('/api/prueba', type='http', csrf=False, auth="public")
+#     def update_order_webhook(self, **kwargs):
+#     return Response(json.dumps({"yes":"i am json"}),content_type='application/json;charset=utf-8',status=200)
