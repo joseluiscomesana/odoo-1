1d9976473900eeb9561d4f0661fbbc60f16cf741
José Luis <joseluis.comesana@Impulsa20>
2021-05-03 15:42:36 +0200

    testButton

--- prueba/models/models.py
+++ prueba/models/models.py
@@ -6,10 +6,16 @@ from odoo import models, fields, api
 class Visit(models.Model):
     _name = 'prueba.visit'
     _description = 'Visit'
+    _inherit = "res.partner"
 
     name = fields.Char(string='Descripción')
     # customer = fields.Char(string='Cliente')
-    customer = fields.Many2one(string='Cliente', comodel_name="res_partner")
+    # customer = fields.Many2one(context='Cliente', comodel_name="res.partner")
+    customer = fields._id = fields.Many2one(string='Cliente', comodel_name='res.partner.name', ondelete='restrict')
     date = fields.Datetime(string='Fecha')
     type = fields.Selection([('P','Presencial'),('W','Whatsapp'),('T','Telefónica')],string='Tipo',required=True)
     done = fields.Boolean(string='Realizada')
+    image = fields.Binary(string="Imagen")
+
+def toggle_state(self):
+    self.done = not self.done
