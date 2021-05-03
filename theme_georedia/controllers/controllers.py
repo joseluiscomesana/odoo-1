ed8410fa794f52364e21c3176b4664efa8c2314f
Antonio García <agarcia@shsconsultores.es>
2021-05-03 13:01:11 +0200

    one controller

--- theme_georedia/controllers/controllers.py
+++ theme_georedia/controllers/controllers.py
@@ -2,6 +2,6 @@
 from odoo import http
 
 class MyModule(http.Controller):
-    @http.route('/my_module/my_module/', auth='public')
+    @http.route('/api/prueba/', auth='public')
     def index(self, **kw):
         return "Hello, world - " + http.request.params['nombre']
