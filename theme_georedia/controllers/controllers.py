65ee970fa0159d691995fa1632cd2709bf085a9a
Antonio García <agarcia@shsconsultores.es>
2021-05-03 13:17:51 +0200

    updates in controllers

--- theme_georedia/controllers/controllers.py
+++ theme_georedia/controllers/controllers.py
@@ -5,3 +5,8 @@ class MyModule(http.Controller):
     @http.route('/api/prueba/', auth='public')
     def index(self, **kw):
         return "Hello, world - " + http.request.params['nombre']
+
+
+    @http.route('/api/prueba', type='http', csrf=False, auth="public")
+    def update_order_webhook(self, **kwargs):
+    return Response(json.dumps({"yes":"i am json"}),content_type='application/json;charset=utf-8',status=200)
