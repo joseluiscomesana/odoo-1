6b6f621077200db320fda154d3f58406dbb72446
Antonio García <agarcia@shsconsultores.es>
2021-05-05 15:23:20 +0200

    dos controladores de prueba

--- informe_geo/controllers/controllers.py
+++ informe_geo/controllers/controllers.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from http import request
+import json
 
 
-# class InformeGeo(http.Controller):
-#     @http.route('/informe_geo/informe_geo/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class InformeGeo(http.Controller):
+    @http.route('/apiweb/', auth='none', type="json", methods=['GET'], csrf=False, cors='*')
+    def index(self, **kw):
+        data = json.loads(request.httprequest.data) # I need raw data
+        return data
+
+
+
+    @http.route('/informe_geo/informe_geo/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
 #     @http.route('/informe_geo/informe_geo/objects/', auth='public')
 #     def list(self, **kw):
