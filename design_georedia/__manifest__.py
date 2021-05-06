80ae07934842b1cd0db83555df04aed1e63e8151
Antonio García <agarcia@shsconsultores.es>
2021-05-06 15:31:10 +0200

    add scripts tag again

--- design_georedia/__manifest__.py
+++ design_georedia/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "SHS Consultores",
+    'website': "http://www.shsconsultores.es",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','website'],
 
     # always loaded
     'data': [
