f4db0c40bbd8d199d8bd457020a55af194eef007
Antonio García <agarcia@shsconsultores.es>
2021-05-06 15:04:19 +0200

    add build angular dist in module

--- design_georedia/controllers/controllers.py
+++ design_georedia/controllers/controllers.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class DesignGeoredia(http.Controller):
-#     @http.route('/design_georedia/design_georedia/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class DesignGeoredia(http.Controller):
+    @http.route('/design_georedia/design_georedia/', auth='public')
+    def index(self, **kw):
+        return http.request.render('design_georedia.index', {})
 
 #     @http.route('/design_georedia/design_georedia/objects/', auth='public')
 #     def list(self, **kw):
