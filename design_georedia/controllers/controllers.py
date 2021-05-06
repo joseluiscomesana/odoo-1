38c69ecd9d14f38adb051a93c5e9ef833482115f
Antonio García <agarcia@shsconsultores.es>
2021-05-06 14:11:19 +0200

    create module design_georedia

---
+++ design_georedia/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class DesignGeoredia(http.Controller):
+#     @http.route('/design_georedia/design_georedia/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/design_georedia/design_georedia/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('design_georedia.listing', {
+#             'root': '/design_georedia/design_georedia',
+#             'objects': http.request.env['design_georedia.design_georedia'].search([]),
+#         })
+
+#     @http.route('/design_georedia/design_georedia/objects/<model("design_georedia.design_georedia"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('design_georedia.object', {
+#             'object': obj
+#         })
