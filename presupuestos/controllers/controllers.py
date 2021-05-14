356fe7edb8b16b71c50a2d2b9e40edd31a4cd61e
José Luis <joseluis.comesana@Impulsa20>
2021-05-14 09:48:30 +0200

    presArt

--- presupuestos/controllers/controllers.py
+++ presupuestos/controllers/controllers.py
@@ -5,21 +5,6 @@ import json
 import datetime
 
 class PresupuestosController(http.Controller):
-    @http.route('/api/presupuestos/lt', auth='public',method=['GET'],website=True)
-    def index(self, **kw):
-        presus = http.request.env['presupuestos.presupuesto']
-        return http.request.render('presupuestos.index',{
-            'objects':presus.search([])
-        })
-    @http.route('/api/presupuestos', auth='public', method=['GET'], csrf=False)
-    def get_presupuestos(self, **kw):
-        try:
-            presupuestos = http.request.env['presupuestos.presupuesto'].sudo().search_read([], ['id', 'ensayo', 'descripcion'])
-            res = json.dumps(presupuestos, ensure_ascii=False).encode('utf-8')
-            return Response(res, content_type='application/json;charset=utf-8', status=200)
-        except Exception as e:
-            return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
-    
     # Datos de un articulo por su id o por su código 
     @http.route('/api/presupuestos/busca/<obj>/', auth='public', website=True)
     def object(self, obj, **kw):
@@ -35,12 +20,31 @@ class PresupuestosController(http.Controller):
             condicion = [(busca,'=',obj)] 
             presupuestos = http.request.env['presupuestos.presupuesto'].sudo().search_read(condicion)
             res = json.dumps(presupuestos,default=fecha).encode('utf-8')
+            return http.request.render('presupuestos.index2',{
+                'objects':res
+            })
+           #  return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
+
+    @http.route('/api/presupuestos/lt', auth='public',method=['GET'],website=True)
+    def index(self, **kw):
+        presus = http.request.env['presupuestos.presupuesto']
+        return http.request.render('presupuestos.index',{
+            'objects':presus.search([])
+        })
+
+    @http.route('/api/presupuestos', auth='public', method=['GET'], csrf=False)
+    def get_presupuestos(self, **kw):
+        try:
+            presupuestos = http.request.env['presupuestos.presupuesto'].sudo().search_read([], ['id', 'ensayo', 'descripcion'])
+            res = json.dumps(presupuestos, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
             return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
     
     # Datos de un articulo por su id o por su código 
-    @http.route('/api/articulo/busca/<obj>/', auth='public', website=True)
+    @http.route('/api/articulo/buscar/<obj>/', auth='public')
     def object(self, obj, **kw):
         try:
             def fecha(o):
